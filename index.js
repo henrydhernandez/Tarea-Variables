edd67e01231dd07951656b45138518c304835ca4
Henry Hernandez <henryhdha@gmail.com>
2024-09-12 19:14:06 -0400

    primer commit

---
+++ index.js
@@ -0,0 +1,77 @@
+// Inicialización de Variables en JavaScript
+// dadas las siguientes 15 variables inicialízalas con el tipo indicado segun su nombre, si es entero, flotante, string, array, objeto. etc.
+
+let edad = 27;
+let cantidadProductos = 500;
+let numeroDeUsuarios = 50;
+
+
+let precio = 1500;
+let temperatura = 35.6;
+let altura = 850.26;
+
+
+let nombre = "Henry";
+let apellido = "Hernández";
+let mensaje = "Bienvenido Henry";
+
+
+let numeros = [85,92,36,41,78,56];
+let nombres = ["Henry", "Hosni", "Alberto", "Jesus", "Mariana", "Monica"];
+let precios = [500, 700, 800, 900, 1200, 1500];
+
+
+let persona = {
+    estatura: 1.60,
+    edad: 27,
+    colorDeOjos: "Marrones",
+};
+let producto = {
+    precio: 50,
+    tipo: "tecnológico",
+    descripcion: "audifonos color negro mate",
+};
+let direccion = {
+    numeroDeCalle: 5,
+    nombreDeCalle: "Solano",
+    nombreEdificio: "Azulejo",
+    piso:7,
+};
+
+
+let esActivo = false;
+let esMayorDeEdad = true;
+let tieneLicencia = true;
+
+// Inicialización de Variables en JavaScript
+//las siguientes variables no siguen la guia de estilo para escribir variables arreglarás siguiendo la guia.
+
+// Enteros
+let totalLibros;
+let numeroPaginas;
+let cantidadArticulos;
+
+// Flotantes
+let pesoPromedio;
+let velocidadMedia;
+let distanciaRecorrida;
+
+// Strings
+let tituloDelLibro;
+let nombreAutor;
+let descripcionCompleta;
+
+// Arrays
+let listaTareas;
+let coloresArray;
+let ciudadesLista;
+
+// Objetos
+let objetoVehiculo;
+let datosEstudiante;
+let infoEmpleado;
+
+// Booleanos
+let esValido;
+let permisoObtenido;
+let disponibleEstado;
---
+++ tarea.js
@@ -0,0 +1,39 @@
+// Imprime en la consola el tipo de cada variable usando la función typeof
+// la tarea es investigar como funciona y que hace typeof asi como agregar la definicion.
+
+/* DEFINICIÓN DE TYPEOF: typeof es un operador JavaScript que al ser llamado sobre una variable, 
+devuelve el tipo de dato que dicha variable contiene. */
+
+// Entero
+let totalLibros = 50;
+
+// Flotante
+let pesoPromedio = 70.5;
+
+// String
+let autorNombre = "Gabriel García Márquez";
+
+// Array
+let colores = ["rojo", "verde", "azul"];
+
+// Objeto
+let vehiculo = {
+  marca: "Toyota",
+  modelo: "Corolla",
+  colores: 'amarrillo'
+};
+
+// Booleano
+let permisoObtenido = true;
+
+console.log (typeof totalLibros);
+console.log(typeof pesoPromedio);
+console.log(typeof autorNombre);
+console.log(typeof colores);
+console.log(typeof vehiculo);
+console.log(typeof permisoObtenido);
+
+/* para ejecutar en consola: abrir consola, ubicarse en la carpeta que contiene el archivo y 
+luego escribir node seguido del nombre del archivo que se desea ejecutar */
+
+                   
